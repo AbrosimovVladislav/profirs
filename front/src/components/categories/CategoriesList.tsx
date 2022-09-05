e3dc3804c10c190886522275bb1225f0dbc7430a
scokic <strahinjacokic@gmail.com>
2022-09-05 08:34:46 +0200

    [2wu116g] Enabled showing only 5 results on categories lists

--- front/src/components/categories/CategoriesList.tsx
+++ front/src/components/categories/CategoriesList.tsx
@@ -1,20 +1,19 @@
-import {Grid} from '@mantine/core';
+import { Grid } from '@mantine/core';
 import React from 'react';
 import ListWithTitle from '../common/ListWithTitle';
-import {useGetCategories} from "../../hooks/useCategories";
+import { useGetCategories } from '../../hooks/useCategories';
 
 const CategoriesList = () => {
-
-  const {categories} = useGetCategories();
+  const { categories } = useGetCategories();
 
   return (
-      <Grid gutter="md">
-        {categories.map(category => (
-            <Grid.Col span={6} lg={3} key={category.id}>
-              <ListWithTitle title={category.title} sections={category.children}/>
-            </Grid.Col>
-        ))}
-      </Grid>
+    <Grid gutter="md">
+      {categories.map((category) => (
+        <Grid.Col span={6} lg={3} key={category.id}>
+          <ListWithTitle title={category.title} category={category} />
+        </Grid.Col>
+      ))}
+    </Grid>
   );
 };
 
